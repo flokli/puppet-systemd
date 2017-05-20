9b9f2ed0988ed546d9a985b6d24689fdcb74720e
Florian Klink <flokli@flokli.de>
2017-05-20 21:27:24 +0200

    systemd::networkd::simple_network: add params for network address, gateway, dns and domains

--- manifests/networkd/simple_network.pp
+++ manifests/networkd/simple_network.pp
@@ -10,6 +10,10 @@ define systemd::networkd::simple_network (
   Optional[String] $match_architecture = undef,
   
   Enum['yes', 'no', 'ipv4', 'ipv6'] $network_dhcp = 'yes',
+  Optional[Array[String]] $network_address = undef,
+  Optional[Array[String]] $network_gateway = undef,
+  Optional[Array[String]] $network_dns = undef,
+  Optional[Array[String]] $network_domains = undef,
   Optional[String] $network_ipv6_token = undef,
   Optional[Boolean] $network_ipv6_accept_router_advertisements = undef,
 
@@ -31,6 +35,10 @@ define systemd::networkd::simple_network (
       }.filter |$k, $v| { $v != undef }},
       {'Network' => {
         'DHCP' => $network_dhcp,
+        'Address' => $network_address,
+        'Gateway' => $network_gateway,
+        'DNS' => $network_dns,
+        'Domains' => $network_domains,
         'IPv6Token' => $network_ipv6_token,
         'IPv6AcceptRouterAdvertisements' => $network_ipv6_accept_router_advertisements,
       }.filter |$k, $v| { $v != undef }}
