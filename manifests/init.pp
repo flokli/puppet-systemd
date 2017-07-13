1a967e3b3fe855e9ed0c55f387e3f0c1ee87b16e
Florian Klink <flokli@flokli.de>
2017-07-13 14:41:58 +0200

    systemd: add checks for <Debian Jessie and <Debian Yakkety

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,33 @@
 class systemd {
-  if($::lsbdistcodename == 'jessie') {
-    include apt
-    include apt::backports
-    apt::pin {'systemd-backports':
-      packages => ['systemd', 'libsystemd0', 'libpam-systemd', 'libapparmor1', 'systemd-sysv', 'udev', 'libudev1', 'ifupdown'],
-      release  => 'jessie-backports',
-      priority => 501,
-      notify   => Exec['apt_update'],
+  if($::operatingsystem == 'Debian' and $::operatingsystemrelease < '8') {
+    fail('At least Debian Jessie required for systemd')
+  }
+  if($::operatingsystem == 'Ubuntu' and $::operatingsystemrelease < '16.10') {
+    fail('At least Ubuntu Yakkety required for systemd')
+  }
+
+  package {'systemd':
+    ensure => latest,
+  }
+
+  if($::osfamily == 'Debian') {
+    # install systemd-sysv on Debian osfamily
+    package {'systemd-sysv':
+      ensure  => latest,
     }
 
-    package {['systemd', 'systemd-sysv']:
-      ensure  => latest,
-      require => Apt::Pin['systemd-backports'],
+    # on jessie, pin systemd from backports
+    if($::lsbdistcodename == 'jessie') {
+      include apt
+      include apt::backports
+      apt::pin {'systemd-backports':
+        packages => ['systemd', 'libsystemd0', 'libpam-systemd', 'libapparmor1', 'systemd-sysv', 'udev', 'libudev1', 'ifupdown'],
+        release  => 'jessie-backports',
+        priority => 501,
+        notify   => Exec['apt_update'],
+      }
+      Apt::Pin['systemd-backports'] -> Package['systemd']
+      Class['apt::update'] -> Package['systemd']
     }
   }
 }
