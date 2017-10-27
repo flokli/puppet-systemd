43e79d933dff76c857ea9b708a9574bc98c66617
Andreas Rammhold <andreas@rammhold.de>
2017-10-27 17:41:11 +0200

    systemd::networkd::simple_network: allow passing of unit file priority

--- manifests/networkd/network.pp
+++ manifests/networkd/network.pp
@@ -1,7 +1,14 @@
 define systemd::networkd::network (
-  String $filename = "60-${title}.network",
+  String $priority = '60',
+  String $filename = "${priority}-${title}.network",
   Array $sections = [],
 ) {
+  validate_integer($priority)
+
+  if $priority =~ /^1/ {
+    fail('Priority may not start with 1 because everything matching /^1/ is ignored inside the managed /etc/systemd/network directory.')
+  }
+
   systemd::networkd::config { $filename:
     sections => $sections,
   }
